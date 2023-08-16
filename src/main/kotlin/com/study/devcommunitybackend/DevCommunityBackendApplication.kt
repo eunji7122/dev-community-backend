71cc2d84cbe5622c5c36d41fa5593ff7463f44b3
eunji7122 <lej7122@gmail.com>
2023-08-16 23:24:04 +0900

    first commit

---
+++ src/main/kotlin/com/study/devcommunitybackend/DevCommunityBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.study.devcommunitybackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevCommunityBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<DevCommunityBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/devcommunitybackend/DevCommunityBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.devcommunitybackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevCommunityBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
